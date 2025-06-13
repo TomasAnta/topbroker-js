490762398262551ab292de285b10b741c0f81803
TomasAnta <tomas.a@commercecore.com>
2025-06-13 21:59:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
         outDir: "dist",
         emptyOutDir: true,
     },
-    publicDir: "public",
+    publicDir: "../public",
     css: {
         preprocessorOptions: {
             scss: {},
